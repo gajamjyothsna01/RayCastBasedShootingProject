8af8ffba176f23da198363eab7e5ad954be0c0c8
Gajam_Jyothsna <jyothsna.gajam@yesgnome.com>
2022-04-19 18:58:12 +0530

    Player Movement is Added.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed;
-    public Rigidbody rb;
-    bool isGrounded = false;
+    public float playerJumpForce;
+     Rigidbody rb;
+    //bool isGrounded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +17,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isGrounded == false)
-        {
+        
             float inputX = Input.GetAxis("Horizontal");
             float inputY = Input.GetAxis("Vertical");
+            transform.Translate(Vector3.forward* inputY* playerSpeed* Time.deltaTime);
+            transform.Translate(Vector3.right * inputX * playerSpeed * Time.deltaTime);
 
-            rb.MovePosition(new Vector3(inputX * playerSpeed, inputY * playerSpeed, 0));
-        }
-        else
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            isGrounded = true;
+            rb.AddForce(Vector3.up * playerJumpForce, ForceMode.Impulse);
         }
-      
+
+        
+
+        //rb.MovePosition(new Vector3(inputX * playerSpeed, inputY * playerSpeed, 0));
+
+
+
 
     }
 }
